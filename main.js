78633a28b34bf89bed9623c5ba4f5f16aba60b44
Vadim220409 <107919314+Vadim220409@users.noreply.github.com>
2023-02-16 19:41:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("EX1")
+for (let a = 3; a <= 20; a++){
+    console.log(a)
+}
+
+
+console.log("EX2")
+for (let b = 10; b <= 20; b++) {
+    console.log(b)
+}
+
+
+
+console.log("EX3")
+let number1 = prompt();
+let number2 = prompt();
+for (let i = number1; i <= number2; i++) {
+    console.log(i);
+}
